1004760c80414de094c8fd6db060d26ec573f7d6
Leandro Marques <leandrojerse@gmail.com>
2025-06-02 06:15:04 -0300

    Controllers and Repositories

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devsuperior.dscommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.dscommerce.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-} 
