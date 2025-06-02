56698ffb3ac4315321a614b787374170a7507ebc
Leandro Marques <leandrojerse@gmail.com>
2025-06-02 05:22:16 -0300

    First Api test

--- src/main/java/com/devsuperior/dscommerce/application/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/application/DscommerceApplication.java
@@ -3,15 +3,18 @@ package com.devsuperior.dscommerce.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.devsuperior.dscommerce.entities")
 @EnableJpaRepositories(basePackages = "com.devsuperior.dscommerce.repositories")
+@ComponentScan(basePackages = "com.devsuperior.dscommerce")
 public class DscommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
+		System.out.println("Aplicação iniciada com sucesso!");
 	}
 
 }
